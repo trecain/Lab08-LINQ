8755d464fe0e5c20f6428957dabac56d7b4da94a
trecain <developedbycain@yahoo.com>
2018-10-21 23:04:16 -0500

    Add project files.

---
+++ Lab08-LINQ/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab08_LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
